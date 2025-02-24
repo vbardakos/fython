972bee285abe0f296e706dd6e3f4e4cda4dc3cc8
Vasileios Bardakos <vasilisbardakos@gmail.com>
2025-02-24 14:41:07 +0200

    test(parser): add assign test

---
+++ parser/parser_test.go
@@ -0,0 +1,64 @@
+package parser
+
+import (
+	// "github.com/vbardakos/fython/ast"
+	"testing"
+
+	"github.com/vbardakos/fython/ast"
+	"github.com/vbardakos/fython/lexer"
+)
+
+func TestStatements(t *testing.T) {
+	input := `
+five = 5
+ten = 10
+seven = 7
+`
+	lxr := lexer.New(input)
+	parser := New(lxr)
+
+	program := parser.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram returns nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("ParseProgram did not get 2 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expIdentifier string
+	}{
+		{"five"},
+		{"ten"},
+		{"seven"},
+	}
+
+	for idx, tt := range tests {
+		stmt := program.Statements[idx]
+		assertAssign(t, stmt, tt.expIdentifier)
+	}
+}
+
+func assertAssign(t *testing.T, s ast.Stmt, name string) {
+	if s.TokenLiteral() != "ASSIGN" {
+		t.Fatalf("Stmt is not Assign. got=%T", s)
+	}
+
+	stmt, ok := s.(*ast.AssignStmt)
+
+	if !ok {
+		t.Fatalf("Stmt is not Assign. got=%T", s)
+	}
+
+	if stmt.Name.Value != name {
+		t.Fatalf("Assign.Name.Value error. exp=%s, got=%s",
+			name, stmt.Name.Value)
+	}
+
+	if stmt.Name.TokenLiteral() != name {
+		t.Fatalf("Assign.Name.TokenLiteral error. exp=%s, got=%s",
+			name, stmt.Name.TokenLiteral())
+	}
+}
