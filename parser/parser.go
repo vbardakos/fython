a791b751ee8f13978f97251cef543d8f7a058f23
Vasileios Bardakos <vasilisbardakos@gmail.com>
2025-02-24 14:40:48 +0200

    feat(parser): init assign parsing

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,7 @@
 package parser
 
 import (
-	// "github.com/vbardakos/fython/ast"
+	"github.com/vbardakos/fython/ast"
 	"github.com/vbardakos/fython/lexer"
 	"github.com/vbardakos/fython/token"
 )
@@ -24,4 +24,32 @@ func New(lxr *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curr = p.peek
 	p.peek = p.lxr.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Stmt{}
+
+	for p.curr.Token != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Stmt {
+	switch p.curr.Token {
+	case token.IDENT:
+		return p.parseAssignStmt()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseAssignStmt() ast.Stmt {
+	return nil
 }
